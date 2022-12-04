647331fa110c703dc7bcda99b5d83c67908bb711
shahed <shahedhamed621@gmail.com>
2022-12-04 21:23:40 +0300

    fix strips

--- app/src/main/java/com/washathomes/apputils/appdefs/Urls.kt
+++ app/src/main/java/com/washathomes/apputils/appdefs/Urls.kt
@@ -3,6 +3,7 @@ package com.washathomes.apputils.appdefs
 class Urls {
     companion object{
         var BASE_URL = "https://wash.hudhudclient.com/Api/"
+        var BASE_URL2 = "https://wash.hudhudclient.com/"
         var STRIPE_URL = "https://wash.hudhudclient.com/Stripe_auth/"
     }
 }
